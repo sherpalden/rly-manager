5b99d68b52733469cd3e71abb9f80227b95523c2
sherpalden <sherpalden369@gmail.com>
2024-08-16 22:15:05 +0545

    chore: remove sock file

--- rly0-data/entrypoint.sh
+++ rly0-data/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo 'Setting up the relayer...'
+rm -f /tmp/relayer.sock
 AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore init
 sleep 1
 AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain 0x2.icon --keystore /home/relayer/.centralized-relay/wallets/icon.json --password Password_123
