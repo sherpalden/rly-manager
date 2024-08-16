b69b8dc9b0aa89be1bd9e49295e0849ea78f5c33
sherpalden <sherpalden369@gmail.com>
2024-08-16 20:23:28 +0545

    feat: init relay manager

---
+++ rly0-data/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo 'Setting up the relayer...'
+AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore init
+sleep 1
+AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain 0x2.icon --keystore /home/relayer/.centralized-relay/wallets/icon.json --password Password_123
+AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain solana --keystore /home/relayer/.centralized-relay/wallets/solana.json --password password
+sleep 3
+echo 'Starting relayer...'
+AWS_ENDPOINT_URL=http://kms:8080 centralized-relay start
