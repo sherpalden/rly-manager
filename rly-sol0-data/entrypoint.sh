c59789d12675cf31f9475fcbd9c7eb23bb18512a
sherpalden <sherpalden369@gmail.com>
2024-08-23 15:53:19 +0545

    add solana chain

--- rly-sol0-data/entrypoint.sh
+++ rly-sol0-data/entrypoint.sh
@@ -3,8 +3,8 @@ echo 'Setting up the relayer...'
 rm -f /tmp/relayer.sock
 AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore init
 sleep 1
-AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain 0x2.icon --keystore /home/relayer/.centralized-relay/wallets/icon-0.json --password Password_123
-AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain solana --keystore /home/relayer/.centralized-relay/wallets/solana-0.json --password password
+AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain 0x2.icon --keystore /home/relayer/wallets/icon-0.json --password Password_123
+AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain solana --keystore /home/relayer/wallets/solana-0.json --password password
 sleep 3
 echo 'Starting relayer...'
 AWS_ENDPOINT_URL=http://kms:8080 centralized-relay start
--- rly-sui0-data/entrypoint.sh
+++ rly-sui0-data/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 echo 'Setting up the relayer...'
 rm -f /tmp/relayer.sock
-AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore init
 sleep 1
 AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain 0x2.icon --keystore /home/relayer/wallets/icon-0.json --password Password_123
 AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain sui-test --keystore /home/relayer/wallets/sui-0.json --password password
