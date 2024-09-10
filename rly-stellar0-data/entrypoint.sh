42991d9a4d1e438eb1b73f313ad55dacbdfd6c07
sherpalden <sherpalden369@gmail.com>
2024-09-10 17:36:02 +0545

    feat: add steller chain

---
+++ rly-stellar0-data/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo 'Setting up the relayer...'
+rm -f /tmp/relayer.sock
+sleep 1
+AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain 0x2.icon --keystore /home/relayer/wallets/icon-0.json --password Password_123
+AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain 0x2.stellar --keystore /home/relayer/wallets/stellar-0 --password password
+sleep 3
+echo 'Starting relayer...'
+AWS_ENDPOINT_URL=http://kms:8080 centralized-relay start
