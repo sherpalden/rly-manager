383bcb021740c7fba511cb22009bf5a599b6d2a1
sherpalden <sherpalden369@gmail.com>
2024-09-25 11:53:34 +0545

    fix: damn

--- rly-sol1-data/entrypoint.sh
+++ rly-sol1-data/entrypoint.sh
@@ -3,7 +3,7 @@ echo 'Setting up the relayer...'
 rm -f /tmp/relayer.sock
 sleep 1
 AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain 0x2.icon --keystore /home/relayer/wallets/icon-1.json --password password
-AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain solana --keystore /home/relayer/wallets/solana-1.json --password password
+AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain solana-test --keystore /home/relayer/wallets/solana-1.json --password password
 sleep 3
 echo 'Starting relayer...'
 AWS_ENDPOINT_URL=http://kms:8080 centralized-relay start
