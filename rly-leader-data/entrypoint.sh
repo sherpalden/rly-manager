50282b172ab3578e58be34f6d1943404f87af528
sherpalden <sherpalden369@gmail.com>
2024-11-15 12:30:47 +0545

    fasdf

--- rly-leader-data/entrypoint.sh
+++ rly-leader-data/entrypoint.sh
@@ -4,7 +4,7 @@ rm -f /tmp/relayer.sock
 sleep 1
 AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain 0x2.icon --keystore /home/relayer/wallets/icon-leader.json --password password
 AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain 0xa869.fuji --keystore /home/relayer/wallets/evm-leader.json --password password
-AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain archway --keystore /home/relayer/wallets/archway-leader.json --password password
+AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain archway --keystore /home/relayer/wallets/archway-leader --password password
 sleep 3
 echo 'Starting relayer...'
 AWS_ENDPOINT_URL=http://kms:8080 centralized-relay start
