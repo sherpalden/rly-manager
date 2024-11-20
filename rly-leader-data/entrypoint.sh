df5bc16d8cf31eb9cda0e36c8966073411c154a3
sherpalden <sherpalden369@gmail.com>
2024-11-20 13:11:13 +0545

    feat: add stellar chain

--- rly-leader-data/entrypoint.sh
+++ rly-leader-data/entrypoint.sh
@@ -5,6 +5,7 @@ sleep 1
 AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain 0x2.icon --keystore /home/relayer/wallets/icon-leader.json --password password
 AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain 0xa869.fuji --keystore /home/relayer/wallets/evm-leader.json --password password
 AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain archway --keystore /home/relayer/wallets/archway-leader --password password
+AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain stellar-test --keystore /home/relayer/wallets/stellar-leader --password password
 sleep 3
 echo 'Starting relayer...'
 AWS_ENDPOINT_URL=http://kms:8080 centralized-relay start
