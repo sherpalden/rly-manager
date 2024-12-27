b730dc20bc37f5be2bceeed7939c97e0b402164c
sherpalden <sherpalden369@gmail.com>
2024-12-27 14:02:55 +0545

    feat: sui solana cluster setup

--- leader-entrypoint.sh
+++ leader-entrypoint.sh
@@ -6,6 +6,8 @@ AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain 0x2.i
 AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain 0xa869.fuji --keystore /home/relayer/wallets/evm-leader.json --password password
 AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain archway --keystore /home/relayer/wallets/archway-leader --password password
 AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain stellar-test --keystore /home/relayer/wallets/stellar-leader --password password
+AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain solana-test --keystore /home/relayer/wallets/solana-leader.json --password password
+AWS_ENDPOINT_URL=http://kms:8080 centralized-relay keystore import --chain sui-test --keystore /home/relayer/wallets/sui-leader.json --password password
 sleep 3
 echo 'Starting relayer...'
 AWS_ENDPOINT_URL=http://kms:8080 centralized-relay start
